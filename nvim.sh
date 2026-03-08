b0f65d7a1e63cc7bbe2c3bc54ad2390b254b4013
Stepan Kosatyi <stepan@kosatyi.com>
2026-03-08 19:23:44 +0200

    update nvim.sh

--- nvim.sh
+++ nvim.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 REPOSITORY_URL="https://github.com/kosatyi/dotfiles"
-REPOSITORY_DIR="/tmp/dotfiles_$(date +%Y%m%d_%H%M%S)"
+SOURCE_DIR="/tmp/dotfiles_$(date +%Y%m%d_%H%M%S)"
 
-echo "Cloning $REPOSITORY_URL into $REPOSITORY_DIR..."
-git clone "$REPOSITORY_URL" "$REPOSITORY_DIR"
+echo "Cloning $REPOSITORY_URL into $SOURCE_DIR..."
+git clone "$REPOSITORY_URL" "$SOURCE_DIR"
 
 # Check if cloning was successful
 if [ $? -eq 0 ]; then
@@ -13,11 +13,11 @@ else
     echo "Error: Git clone failed."
 fi
 
-REPOSITORY_NVIM_DIR="$REPOSITORY_DIR/nvim"
+SOURCE_NVIM_DIR="$SOURCE_DIR/nvim"
 NVIM_CONFIG_DIR="$HOME/.config/nvim"
 
-if [ ! -d "$REPOSITORY_NVIM_DIR" ]; then
-    echo "Error: folder $REPOSITORY_NVIM_DIR not found"
+if [ ! -d "$SOURCE_NVIM_DIR" ]; then
+    echo "Error: folder $SOURCE_NVIM_DIR not found"
     exit 1
 fi
 
@@ -28,13 +28,13 @@ fi
 
 # Move files
 mkdir -p "$HOME/.config"
-cp -r "$REPOSITORY_NVIM_DIR" "$NVIM_CONFIG_DIR"
+cp -r "$SOURCE_NVIM_DIR" "$NVIM_CONFIG_DIR"
 
 if [ $? -eq 0 ]; then
   echo "Files successfully copied to $NVIM_CONFIG_DIR"
   # Optional: Clean up the temporary directory
-  rm -rf "$REPOSITORY_DIR"
-  echo "Temporary directory $REPOSITORY_DIR removed."
+  rm -rf "$SOURCE_DIR"
+  echo "Temporary directory $SOURCE_DIR removed."
 else
   echo "Error: Failed to copy files."
 fi
