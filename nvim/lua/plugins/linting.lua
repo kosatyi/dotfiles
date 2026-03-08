3f8f3fb5f81cf7ebb67e4c7ff0a62d798ddca966
Stepan Kosatyi <stepan@kosatyi.com>
2026-03-08 19:59:47 +0200

    update

--- nvim/lua/plugins/linting.lua
+++ nvim/lua/plugins/linting.lua
@@ -1,14 +1,11 @@
 return {
-  -- 1. Додаємо djlint у список автоматичного встановлення Mason
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, { "djlint" })
     end,
   },
-
-  -- 2. Налаштовуємо nvim-lint для запуску djlint на файлах nunjucks
   {
     "mfussenegger/nvim-lint",
     opts = {
