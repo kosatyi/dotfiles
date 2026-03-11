9f2597623ad93716c3104b7282cad5221cd619a4
Stepan Kosatyi <stepan@kosatyi.com>
2026-03-11 22:51:21 +0200

    update config add ghostty config

--- install.sh
+++ install.sh
@@ -9,34 +9,43 @@ git clone "$REPOSITORY_URL" "$SOURCE_DIR"
 # Check if cloning was successful
 if [ $? -eq 0 ]; then
     echo "Cloning successful. Copying files to home directory..."
+    # Clean git directory
+    echo "Clean $SOURCE_DIR/.git directory"
+    rm -rf "$SOURCE_DIR/.git"
 else
     echo "Error: Git clone failed."
 fi
 
-SOURCE_NVIM_DIR="$SOURCE_DIR/nvim"
-NVIM_CONFIG_DIR="$HOME/.config/nvim"
-
-if [ ! -d "$SOURCE_NVIM_DIR" ]; then
-    echo "Error: folder $SOURCE_NVIM_DIR not found"
+copy_dot_files() {
+if [ ! -d "$2" ]; then
+    echo "Error: folder $2 not found"
     exit 1
 fi
-
-if [ -d "$NVIM_CONFIG_DIR" ]; then
-    echo "Create backup of existing neovim config"
-    mv "$NVIM_CONFIG_DIR" "${NVIM_CONFIG_DIR}_backup_$(date +%Y%m%d_%H%M%S)"
+if [ -d "$2" ]; then
+    echo "Create backup of existing $1 config"
+    mv "$3" "${3}_backup_$(date +%Y%m%d_%H%M%S)"
 fi
-
-# Move files
+echo "Copy files from $2 to $3"
 mkdir -p "$HOME/.config"
-cp -r "$SOURCE_NVIM_DIR" "$NVIM_CONFIG_DIR"
-rm -rf "$NVIM_CONFIG_DIR/.git"
+cp -r "$2" "$3"
 if [ $? -eq 0 ]; then
-  echo "Files successfully copied to $NVIM_CONFIG_DIR"
-  # Optional: Clean up the temporary directory
-  rm -rf "$SOURCE_DIR"
-  echo "Temporary directory $SOURCE_DIR removed."
+  echo "Files successfully copied to $3"
 else
   echo "Error: Failed to copy files."
 fi
+}
 
-echo "Neovim config updated"
+remove_source_dir(){
+rm -rf "$1"
+echo "Temporary directory $1 removed."
+}
+
+copy_dot_files "NeoVim" "$SOURCE_DIR/nvim" "$HOME/.config/nvim"
+copy_dot_files "Ghostty" "$SOURCE_DIR/ghostty" "$HOME/.config/ghostty"
+
+remove_source_dir $SOURCE_DIR
+
+
+
+
+
