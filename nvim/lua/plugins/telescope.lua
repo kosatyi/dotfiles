175e6fbb5b38e730ef1ba0b005b68f3628b149d0
Stepan Kosatyi <stepan@kosatyi.com>
2026-03-11 02:17:15 +0200

    update

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,22 +1,26 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-project.nvim",
-      "vinnymeller/telescope-npm.nvim",
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-project.nvim",
+    "vinnymeller/telescope-npm.nvim",
+  },
+  keys = {
+    { "<leader>zp", "<cmd>Telescope project<cr>",     desc = "Projects" },
+    { "<leader>zn", "<cmd>Telescope npm scripts<cr>", desc = "NPM Scripts" },
+    {
+      "<leader>zz",
+      function()
+        require("telescope.builtin").find_files({ cwd = vim.fn.expand("%:p:h") })
+      end,
+      desc = "Find Files (Buffer Dir)",
     },
+  },
+  opts = {
     defaults = {
       vimgrep_arguments = {
-        "rg",
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--smart-case",
-        "--glob",
-        "!node_modules/*",
+        "rg", "--color=never", "--no-heading", "--with-filename",
+        "--line-number", "--column", "--smart-case", "--glob", "!node_modules/*",
       },
     },
     pickers = {
@@ -24,33 +28,17 @@ return {
         find_command = { "fd", "--type", "f", "--strip-cwd-prefix", "--exclude", "node_modules" },
       },
     },
-    keys = {
-      { "<leader>zp", "<cmd>Telescope project<cr>",     desc = "Projects" },
-      { "<leader>zn", "<cmd>Telescope npm scripts<cr>", desc = "NPM Scripts" },
-      {
-        "<leader>zz",
-        function()
-          require("telescope.builtin").find_files({
-            cwd = vim.fn.expand("%:p:h"),
-          })
-        end,
-        desc = "Find Files (Current Buffer Dir)",
+    extensions = {
+      project = {
+        base_dirs = { "~/flitt/projects", "~/projects", "~/www" },
+        hidden_files = true,
       },
     },
-    opts = function(_, opts)
-      local telescope = require("telescope")
-      telescope.load_extension("project")
-      telescope.load_extension("npm")
-      opts.extensions = {
-        project = {
-          base_dirs = {
-            { "~/flitt/projects" },
-            { "~/projects" },
-            { "~/www" },
-          },
-          hidden_files = true, -- показувати .git, .env тощо
-        },
-      }
-    end,
-  }
+  },
+  config = function(_, opts)
+    local telescope = require("telescope")
+    telescope.setup(opts)
+    telescope.load_extension("project")
+    telescope.load_extension("npm")
+  end,
 }
