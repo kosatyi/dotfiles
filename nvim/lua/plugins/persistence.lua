4a6ad74cf4a89586e1308002e94b1117e2ccef29
Stepan Kosatyi <stepan@kosatyi.com>
2026-03-12 13:35:54 +0200

    update

--- nvim/lua/plugins/persistence.lua
+++ nvim/lua/plugins/persistence.lua
@@ -6,11 +6,20 @@ return {
     -- dir = vim.fn.expand(vim.fn.stdpath("state") .. "/sessions/"), -- шлях до сесій
     -- options = { "buffers", "curdir", "tabpages", "winsize" }, -- що зберігати
   },
-  -- Стандартні гарячі клавіші LazyVim:
-  keys = {
+   keys = {
     { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
     { "<leader>qS", function() require("persistence").select() end, desc = "Select Session" },
     { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
     { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+     "<leader>qX", function()
+        local session_file = require("persistence").current()
+        if session_file then
+            os.remove(session_file)
+            print("Сесію видалено: " .. session_file)
+        else
+            print("Активної сесії не знайдено")
+        end
+    end,
+    desc = "Delete Current Session",
   },
 }
