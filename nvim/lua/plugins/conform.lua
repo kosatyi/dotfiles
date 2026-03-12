30213db3b7788f170f6cdc0a7df671138c23827c
Stepan Kosatyi <stepan@kosatyi.com>
2026-03-12 15:28:40 +0200

    update

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -2,6 +2,7 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
+      html = { "prettier" , stop_after_first = true},
       scss = { "prettier", stop_after_first = true },
       css = { "prettier", stop_after_first = true },
       javascript = { "prettier", stop_after_first = true },
