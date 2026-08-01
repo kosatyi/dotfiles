4d5eb99e350f53fa78a46b3522a064727217831e
Stepan <stepan@MacBook-Air-Stepan.local>
2026-08-01 16:34:36 +0300

    update plugins

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,41 +1,37 @@
-return { {
-  'folke/snacks.nvim',
-  priority = 1000,
-  lazy = false,
-  opts = {
-    picker = {
-      sources = {
-        explorer = {
-          layout = {
-            layout = {
-              width = 30,
-            },
-            auto_hide = { 'input' },
+return {
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      dashboard = {
+        sections = {
+          {
+            section = "header",
+            padding = 0,
           },
+          {
+            title = false,
+            section = "keys",
+            padding = 1,
+          },
+          {
+            title = "Recent Files",
+            section = "recent_files",
+            cwd = true,
+            limit = 3,
+            padding = 1,
+          },
+          {
+            title = "Projects",
+            section = "projects",
+            limit = 2,
+            padding = 1,
+          },
+          { section = "startup" },
         },
+        preset = { header = "NVIM" },
       },
     },
-    dashboard = {
-      sections = { {
-        section = 'header',
-        padding = 0,
-      }, {
-        title = false,
-        section = 'keys',
-        padding = 1,
-      }, {
-        title = 'Recent Files',
-        section = 'recent_files',
-        cwd = true,
-        limit = 3,
-        padding = 1,
-      }, {
-        title = 'Projects',
-        section = 'projects',
-        limit = 2,
-        padding = 1,
-      }, { section = 'startup' } },
-      preset = { header = 'NVIM' },
-    },
   },
-} }
+}
