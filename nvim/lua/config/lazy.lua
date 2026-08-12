6cc57ea502f3d3da18ae92a3db93a52cb91d7999
Stepan Kosatyi <stepan@kosatyi.com>
2026-08-12 15:52:17 +0300

    update

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,10 +1,10 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo(
-      { { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' }, { out, 'WarningMsg' }, { '\nPress any key to exit...' } },
+      { { "Failed to clone lazy.nvim:\n", "ErrorMsg" }, { out, "WarningMsg" }, { "\nPress any key to exit..." } },
       true,
       {}
     )
@@ -14,17 +14,21 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-  spec = { {
-    'LazyVim/LazyVim',
-    import = 'lazyvim.plugins',
-  }, { import = 'lazyvim.plugins.extras.editor.neo-tree' },{ import = 'plugins' } },
+require("lazy").setup({
+  spec = {
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+    },
+    { import = "lazyvim.plugins.extras.editor.neo-tree" },
+    { import = "plugins" },
+  },
   defaults = {
     lazy = false,
     version = false,
   },
   install = {
-    colorscheme = { 'tokyonight' },
+    colorscheme = { "tokyonight" },
   },
   checker = {
     enabled = true,
@@ -32,7 +36,7 @@ require('lazy').setup({
   },
   performance = {
     rtp = {
-      disabled_plugins = { 'gzip', 'tarPlugin', 'tohtml', 'tutor', 'zipPlugin' },
+      disabled_plugins = { "gzip", "tarPlugin", "tohtml", "tutor", "zipPlugin" },
     },
   },
 })
